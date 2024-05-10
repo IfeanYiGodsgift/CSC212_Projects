bc69b775a1070a37ba7bf7a12a9ce93cad3943ab
IfeanYiGodsgift <godsgift.ifeanyi@pau.edu.ng>
2024-05-10 20:47:36 +0100

    OpenTrig

---
+++ OpenglProject/Main.cpp
@@ -0,0 +1,130 @@
+#include<iostream>
+#include<glad/glad.h>
+#include<GLFW/glfw3.h>
+using namespace std;
+
+const char* vertexShaderSource = "#version 330 core\n"
+"layout (location = 0) in vec3 aPos;\n"
+"void main()\n"
+"{\n"
+"	gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
+"}\0";
+const char* fragmentShaderSource = "#version 330 core\n"
+"out vec4 FragColor; \n"
+"void main()\n"
+"{\n"
+"	FragColor = vec4(0.8f, 0.3f, 0.02f, 1.0f);\n"
+"}\n\0";
+
+
+
+int main() {
+	
+	glfwInit();
+
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+	GLfloat vertices[] =
+	{
+		-0.5f, -0.5f * float(sqrt(3)) / 3, 0.0f,
+		0.5f, -0.5f * float(sqrt(3)) / 3, 0.0f,
+		0.0f, 0.5f * float(sqrt(3)) * 2 / 3, 0.0f,
+	};
+
+	GLFWwindow* window = glfwCreateWindow(800, 800, "GLFW Window", NULL, NULL);
+	if (!window) {
+		cout << "Failed to create GLFW window" << endl;
+		glfwTerminate();
+		return -1;
+	}
+
+	//// Create the second window
+	//GLFWwindow* window2 = glfwCreateWindow(800, 800, "GLFW Window 2", NULL, NULL);
+	//if (!window2) {
+	//	cout << "Failed to create second GLFW window" << endl;
+	//	glfwTerminate();
+	//	return -1;
+	//}
+
+
+	glfwMakeContextCurrent(window);
+	gladLoadGL();
+	glViewport(0, 0, 800, 800);
+
+	GLuint vertexShader = glCreateShader(GL_VERTEX_SHADER);
+	glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
+	glCompileShader(vertexShader);
+
+	GLuint fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+	glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL);
+	glCompileShader(fragmentShader);
+
+	GLuint shaderProgram = glCreateProgram();
+	glAttachShader(shaderProgram, vertexShader);
+	glAttachShader(shaderProgram, fragmentShader);
+	glLinkProgram(shaderProgram);
+
+	glDeleteShader(vertexShader);
+	glDeleteShader(fragmentShader);
+
+	GLuint VAO,VBO;
+	glGenVertexArrays(1, &VAO);
+	glGenBuffers(1, &VBO);
+	glBindVertexArray(VAO);
+	glBindBuffer(GL_ARRAY_BUFFER, VBO);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+	glEnableVertexAttribArray(0);
+
+	glBindBuffer(GL_ARRAY_BUFFER, 0);
+	glBindVertexArray(0);
+
+
+
+
+	glClearColor(0.07f, 0.13f, 0.17f, 1.0f);
+	glClear(GL_COLOR_BUFFER_BIT);
+	glfwSwapBuffers(window);
+
+
+
+	//// Make the second window's context current
+	//glfwMakeContextCurrent(window2);
+	//// Load OpenGL function pointers for the second window
+	//gladLoadGL();
+	//// Set viewport size for the second window
+	//glViewport(0, 0, 800, 800);
+	//// Set clear color to black (R:0, G:0, B:0, Alpha:1) for the second window
+	//glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
+	//// Clear the color buffer for the second window
+	//glClear(GL_COLOR_BUFFER_BIT);
+	//// Swap the front and back buffers for the second window
+	//glfwSwapBuffers(window2);
+	
+
+
+	while (!glfwWindowShouldClose(window)) {//&& !glfwWindowShouldClose(window2)
+		glClearColor(0.07f, 0.13f, 0.17f, 1.0f);
+		glClear(GL_COLOR_BUFFER_BIT);
+		glUseProgram(shaderProgram);
+		glBindVertexArray(VAO);
+		glDrawArrays(GL_TRIANGLES, 0, 3);
+		glfwSwapBuffers(window);
+
+		glfwPollEvents();
+	}
+
+	glDeleteVertexArrays(1, &VAO);
+	glDeleteBuffers(1, &VBO);
+	glDeleteProgram(shaderProgram);
+
+	glfwDestroyWindow(window);
+	//glfwDestroyWindow(window2);
+	glfwTerminate();
+	return 0;
+}
+
+// if you uncomment the second window triangle function ceases 
